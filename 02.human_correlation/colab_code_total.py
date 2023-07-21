c51ee5a05dc2c5860a12d349ac8ca1629315037c
conch in cradle <33627638+conchincradle@users.noreply.github.com>
2023-07-22 02:46:02 +0800

    Update colab_code_total.py

--- 02.human_correlation/colab_code_total.py
+++ 02.human_correlation/colab_code_total.py
@@ -57,21 +57,3 @@ for subject in range(14):
   plt.savefig("human_fig/"+str(subject)+"_human_corr_total.png")
   plt.show()
   # print(nums)
-
-import os, tarfile
-
-import os
-from google.colab import files
-
-def make_targz_one_by_one(output_filename, source_dir):
-  tar = tarfile.open(output_filename,"w")
-  for root,dir_name,files_list in os.walk(source_dir):
-    for file in files_list:
-      pathfile = os.path.join(root, file)
-      tar.add(pathfile)
-  tar.close()
-
-  files.download(output_filename)
-
-
-make_targz_one_by_one('human_fig_total.tar', './human_fig//')
